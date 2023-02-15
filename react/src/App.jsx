d7af83f7e9690f648cd796822dad996acb74b39d
Rubby A. Perez Lorenzo <rubbyantonio@gmail.com>
2023-02-15 02:53:30 -0400

    Laravel React Tailwind

---
+++ react/src/App.jsx
@@ -0,0 +1,14 @@
+import { BeakerIcon } from '@heroicons/react/24/solid'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="bg-purple-500">
+    <h1>App.jsx</h1>
+    <BeakerIcon className="w-6"/>
+    </div>
+  )
+}
+
+export default App
